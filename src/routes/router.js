4d0d07af6226063d0b946962df82034821b1c5c1
AnnisaMairaAnastasya <annisamairaanastasya@student.uir.ac.id>
2025-07-07 16:52:22 +0700

    fix import in router

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,12 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 import Transaksi from "@/views/Transaksi.vue";
 import Dashboard from "@/views/Dashboard.vue";
 import Riwayat from "@/views/Riwayat.vue";
-import pengerjaan from "@/views/pengerjaan.vue";
+import Pengerjaan from "@/views/Pengerjaan.vue";
 
 const routes = [
     {path: '/', name: 'dashboard', component: Dashboard},
     {path: '/transaksi', name: 'transaksi', component: Transaksi},
-    {path: '/pengerjaan', name:'pengerjaan', component: pengerjaan},
+    {path: '/pengerjaan', name:'pengerjaan', component: Pengerjaan},
     {path: '/riwayat', name:'profile', component: Riwayat}
 ]
 
